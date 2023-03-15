8242deaee9bb5b3c64417fab1693fda3861f22d5
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-15 21:38:45 +0530

    Added:code to create a BST by adding key

---
+++ bst/BinaryMain.java
@@ -0,0 +1,13 @@
+package bst;
+
+public class BinaryMain {
+    public static void main(String[] args) {
+
+        MyBinaryTree binary = new MyBinaryTree<>();
+
+        binary.add(56);
+        binary.add(30);
+        binary.add(70);
+
+        binary.traversal();    }
+}
