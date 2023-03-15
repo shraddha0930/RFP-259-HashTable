8614eb34af72cdf304611e7b1b424f5f6eaf03e6
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-15 22:22:12 +0530

    Added:code to check if all are adding with using size method in binary tree

--- bst/BinaryMain.java
+++ bst/BinaryMain.java
@@ -5,9 +5,25 @@ public class BinaryMain {
 
         MyBinaryTree binary = new MyBinaryTree<>();
 
-        binary.add(56);
-        binary.add(30);
-        binary.add(70);
+//        binary.add(56);
+//        binary.add(30);
+//        binary.add(70);
+//Adding values to BST
+        for (int i = 0; i < binary.Value.length; i++) {
+            binary.add(binary.Value[i]);
+        }
+        binary.traversal();
+        //Calculating the size of BST
+        binary.calculateSize();
 
-        binary.traversal();    }
+        //Calculating the height of BST
+        System.out.println("The height of the BST is : " + binary.calculateHeight(binary.root));
+
+        //Checking if all values are present in BST
+        boolean[] flag = new boolean[binary.Value.length];
+        for (int i = 0; i < binary.Value.length; i++) {
+            flag[i] = binary.ifNodeExists(binary.root, binary.Value[i]);
+            System.out.println(binary.Value[i] + " is present |  " + flag[i]);
+        }
+    }
 }
